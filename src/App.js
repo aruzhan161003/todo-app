5ead8260fa2a983df20c11dbc507a84df5b2b137
Аружан Амангельди <aruzanamangeldi@MacBook-Pro-Aruzan-2.local>
2022-12-09 17:30:56 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,41 @@
 import ToDoInput from "./components/ToDoInput";
 import "./styles/style.css";
 import { AiFillDelete, AiFillEdit, AiFillFileAdd } from "react-icons/ai";
+import { useState } from "react";
 
 function App() {
+  // todo = 'Buy Milk'
+  // todos = ['Buy Milk', 'Buy Bread', ...]
+
+  // todo = { id: 1, text: 'Buy Milk' }
+  // todos = [ { id: 1, text: 'Buy Milk' }, { id: 2, text: 'Buy Bread' }, ... ]
+
+  const [todos, setTodos] = useState([
+    { id: 1, text: "Buy Milk" },
+    { id: 2, text: "Buy Bread" },
+    { id: 3, text: "Buy Bread" },
+    { id: 4, text: "Buy Bread" },
+  ]);
+  const [inputValue, setInputValue] = useState("");
+
+  const handleOnInputChanged = (event) => {
+    let newValue = event.target.value;
+    setInputValue(newValue);
+  };
+
+  const handleCreateToDo = () => {
+    // let newId = todos[todos.length - 1]["id"] + 1;
+    let newId = Math.floor(Math.random() * 10000000000000000000);
+    let newTodo = { id: newId, text: inputValue };
+    let updatedTodos = [...todos, newTodo];
+    setTodos(updatedTodos);
+    setInputValue("");
+  };
+
+  const handleDeleteToDo = (todoId) => {
+    let updatedTodos = todos.filter((todo) => todo["id"] != todoId);
+    setTodos(updatedTodos);
+  };
   return (
     <div className="App">
       <nav>
@@ -10,23 +43,32 @@ function App() {
       </nav>
       <div className="todo-container">
         <div className="todo-input">
-          <input placeholder="Write your TODO" />
-          <button className="btn create">
+          <input
+            onChange={(event) => handleOnInputChanged(event)}
+            value={inputValue}
+            placeholder="Write your TODO"
+          />
+          <button className="btn create" onClick={handleCreateToDo}>
             <AiFillFileAdd />
           </button>
         </div>
         <div className="todo-list">
-          <div className="todo">
-            <p>Buy milk</p>
-            <div className="todo-actions">
-              <button className="btn edit">
-                <AiFillEdit />
-              </button>
-              <button className="btn delete">
-                <AiFillDelete />
-              </button>
+          {todos.map((todo) => (
+            <div className="todo">
+              <p>{todo["text"]}</p>
+              <div className="todo-actions">
+                <button className="btn edit">
+                  <AiFillEdit />
+                </button>
+                <button
+                  onClick={() => handleDeleteToDo(todo["id"])}
+                  className="btn delete"
+                >
+                  <AiFillDelete />
+                </button>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>
